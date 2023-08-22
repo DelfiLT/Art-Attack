65b83d07f1fc89101897a6a2bb082ce2637d08aa
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-08-21 22:39:26 -0300

    Platform changes

--- Assets/Scripts/platform.cs
+++ Assets/Scripts/platform.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class platform : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject ice;
-    [SerializeField]
-    private GameObject water;
+    [Header("Platform types")]
+    [SerializeField] private GameObject ice;
+    [SerializeField] private GameObject water;
+    [Header("Platform values")]
+    [SerializeField] private float changeTime;
 
     void Start()
     {
@@ -34,22 +35,18 @@ public class platform : MonoBehaviour
 
     IEnumerator ChangeToIce()
     {
-        Debug.Log("Elemento cambiado a hielo");
         ice.SetActive(true);
         water.SetActive(false);
-        yield return new WaitForSeconds(10f);
-        Debug.Log("Volvio a su estado");
+        yield return new WaitForSeconds(changeTime);
         ice.SetActive(false);
         water.SetActive(true);
     }
 
     IEnumerator ChangeToWater()
     {
-        Debug.Log("Elemento cambiado a awa");
         ice.SetActive(false);
         water.SetActive(true);
-        yield return new WaitForSeconds(10f);
-        Debug.Log("Volvio a su estado");
+        yield return new WaitForSeconds(changeTime);
         ice.SetActive(true);
         water.SetActive(false);
     }

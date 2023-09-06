871c86bd66beab4b10749ed358c4d2e3b3513c17
Delfina Tomasino <tomasinodelfina@gmail.com>
2023-09-06 14:46:57 -0300

    Cat Animations

--- Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
+++ Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ namespace TarodevController {
         private bool _active;
         void Awake() => Invoke(nameof(Activate), 0.5f);
         void Activate() =>  _active = true;
-        
+
         private void Update() {
             if(!_active) return;
             Velocity = (transform.position - _lastPosition) / Time.deltaTime;
